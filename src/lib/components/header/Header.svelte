4c4015b5a9a83ce88e7ffd5c9dbedb2f74716cc7
Jordan Rodrigues <96402459+j0rdanR@users.noreply.github.com>
2023-09-06 22:04:21 +0800

    fix: rename prims algo to prims playground

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -7,7 +7,7 @@
 <header class="h-12 min-h-[48px] pl-3 pr-1 flex items-center justify-between bg-white border-b">
   <p class="flex gap-1 select-none">
     <span class="font-bold text-blue-500">Prims</span>
-    <span class="text-gray-700">Algorithm</span>
+    <span class="text-gray-700">Playground</span>
   </p>
 
   <div class="flex gap-0.5">
